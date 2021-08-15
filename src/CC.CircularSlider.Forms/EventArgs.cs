fff50e74a892d27a3100c0f0a3dddaeb04cb18b5
Ilija Boshkov <ilija@codechem.com>
2021-08-16 00:25:38 +0200

    Adding untested support for drag end event, should address #2

---
+++ src/CC.CircularSlider.Forms/EventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CC
+{
+    public class DragEndEventArgs
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public DragEndEventArgs()
+        {
+        }
+    }
+}
